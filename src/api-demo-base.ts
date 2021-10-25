972fdca53e292b1bbc6abf74da7a8f1f36f7b7b5
web-padawan <iamkulykov@gmail.com>
2021-10-25 11:54:35 +0300

    refactor: simplify docs and demo base

--- src/api-demo-base.ts
+++ src/api-demo-base.ts
@@ -1,36 +1,72 @@
 import { LitElement, html, TemplateResult } from 'lit';
 import { property } from 'lit/decorators/property.js';
 import { until } from 'lit/directives/until.js';
+import { EMPTY_ELEMENT } from './lib/constants.js';
 import { ElementPromise } from './lib/types.js';
 import { ApiViewerMixin, emptyDataWarning } from './api-viewer-mixin.js';
-import './api-demo-content.js';
+import './api-viewer-demo.js';
 
 async function renderDemo(
   jsonFetched: ElementPromise,
+  onSelect: (e: CustomEvent) => void,
   selected?: string,
   id?: number,
   exclude = ''
 ): Promise<TemplateResult> {
   const elements = await jsonFetched;
 
-  const index = elements.findIndex((el) => el.name === selected);
+  if (!elements.length) {
+    return emptyDataWarning;
+  }
 
-  return elements.length
-    ? html`
-        <api-demo-content
-          .elements="${elements}"
-          .selected="${index >= 0 ? index : 0}"
-          .exclude="${exclude}"
-          .vid="${id}"
-        ></api-demo-content>
-      `
-    : emptyDataWarning;
+  const index = selected ? elements.findIndex((el) => el.name === selected) : 0;
+
+  const { name, properties, slots, events, cssProperties } = {
+    ...EMPTY_ELEMENT,
+    ...(elements[index] || {})
+  };
+
+  // TODO: analyzer should sort CSS custom properties
+  const cssProps = (cssProperties || []).sort((a, b) =>
+    a.name > b.name ? 1 : -1
+  );
+
+  return html`
+    <header part="header">
+      <div part="header-title">&lt;${name}&gt;</div>
+      <nav>
+        <label part="select-label">
+          <select
+            @change="${onSelect}"
+            .value="${String(selected)}"
+            ?hidden="${elements.length === 1}"
+            part="select"
+          >
+            ${elements.map(
+              (tag) => html`<option value="${tag.name}">${tag.name}</option>`
+            )}
+          </select>
+        </label>
+      </nav>
+    </header>
+    <api-viewer-demo
+      .name="${name}"
+      .props="${properties}"
+      .slots="${slots}"
+      .events="${events}"
+      .cssProps="${cssProps}"
+      .exclude="${exclude}"
+      .vid="${id}"
+    ></api-viewer-demo>
+  `;
 }
 
 let id = 0;
 
 export class ApiDemoBase extends ApiViewerMixin(LitElement) {
   @property({ type: String, attribute: 'exclude-knobs' }) excludeKnobs?: string;
+
+  @property({ type: Number }) idx = 0;
 
   protected _id?: number;
 
@@ -43,8 +79,18 @@ export class ApiDemoBase extends ApiViewerMixin(LitElement) {
   protected render(): TemplateResult {
     return html`
       ${until(
-        renderDemo(this.jsonFetched, this.selected, this._id, this.excludeKnobs)
+        renderDemo(
+          this.jsonFetched,
+          this._onSelect,
+          this.selected,
+          this._id,
+          this.excludeKnobs
+        )
       )}
     `;
+  }
+
+  private _onSelect(e: CustomEvent): void {
+    this.selected = String((e.target as HTMLSelectElement).value);
   }
 }
--- src/api-docs-base.ts
+++ src/api-docs-base.ts
@@ -1,29 +1,81 @@
 import { LitElement, html, TemplateResult } from 'lit';
 import { until } from 'lit/directives/until.js';
+import { EMPTY_ELEMENT } from './lib/constants.js';
+import { parse } from './lib/markdown.js';
 import { ElementPromise } from './lib/types.js';
 import { ApiViewerMixin, emptyDataWarning } from './api-viewer-mixin.js';
-import './api-docs-content.js';
+import './api-viewer-docs.js';
 
 async function renderDocs(
   jsonFetched: ElementPromise,
+  onSelect: (e: CustomEvent) => void,
   selected?: string
 ): Promise<TemplateResult> {
   const elements = await jsonFetched;
 
-  const index = elements.findIndex((el) => el.name === selected);
+  if (!elements.length) {
+    return emptyDataWarning;
+  }
 
-  return elements.length
-    ? html`
-        <api-docs-content
-          .elements="${elements}"
-          .selected="${index >= 0 ? index : 0}"
-        ></api-docs-content>
-      `
-    : emptyDataWarning;
+  const index = selected ? elements.findIndex((el) => el.name === selected) : 0;
+
+  const {
+    name,
+    description,
+    properties,
+    attributes,
+    slots,
+    events,
+    cssParts,
+    cssProperties
+  } = { ...EMPTY_ELEMENT, ...(elements[index] || {}) };
+
+  // TODO: analyzer should sort CSS custom properties
+  const cssProps = (cssProperties || []).sort((a, b) =>
+    a.name > b.name ? 1 : -1
+  );
+
+  return html`
+    <header part="header">
+      <div part="header-title">&lt;${name}&gt;</div>
+      <nav>
+        <label part="select-label">
+          <select
+            @change="${onSelect}"
+            .value="${String(selected)}"
+            ?hidden="${elements.length === 1}"
+            part="select"
+          >
+            ${elements.map(
+              (tag) => html`<option value="${tag.name}">${tag.name}</option>`
+            )}
+          </select>
+        </label>
+      </nav>
+    </header>
+    <div ?hidden="${description === ''}" part="docs-description">
+      ${parse(description)}
+    </div>
+    <api-viewer-docs
+      .name="${name}"
+      .props="${properties}"
+      .attrs="${attributes}"
+      .events="${events}"
+      .slots="${slots}"
+      .cssParts="${cssParts}"
+      .cssProps="${cssProps}"
+    ></api-viewer-docs>
+  `;
 }
 
 export class ApiDocsBase extends ApiViewerMixin(LitElement) {
   protected render(): TemplateResult {
-    return html`${until(renderDocs(this.jsonFetched, this.selected))}`;
+    return html`${until(
+      renderDocs(this.jsonFetched, this._onSelect, this.selected)
+    )}`;
+  }
+
+  private _onSelect(e: CustomEvent): void {
+    this.selected = String((e.target as HTMLSelectElement).value);
   }
 }
