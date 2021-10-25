2447101b51a1aafe0fb28add9ff7a98de17166c3
web-padawan <iamkulykov@gmail.com>
2021-10-25 09:45:31 +0300

    refactor: simplify demo event log rendering

---
+++ src/lib/demo-events.ts
@@ -0,0 +1,28 @@
+import { html, nothing, TemplateResult } from 'lit';
+
+interface EventDetail {
+  value: string | number | boolean | null | undefined;
+}
+
+const renderDetail = (detail: EventDetail): string => {
+  const result = detail;
+  const undef = 'undefined';
+  if ('value' in detail && detail.value === undefined) {
+    result.value = undef;
+  }
+  return ` detail: ${JSON.stringify(detail).replace(`"${undef}"`, undef)}`;
+};
+
+export const renderEvents = (log: CustomEvent[]): TemplateResult =>
+  html`
+    ${log.map((event) => {
+      return html`
+        <p part="event-record">
+          event:
+          "${event.type}".${event.detail == null
+            ? nothing
+            : renderDetail(event.detail)}
+        </p>
+      `;
+    })}
+  `;
