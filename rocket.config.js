f1b1a3d9a5d7628063167dae5e62e6fd0755a119
web-padawan <iamkulykov@gmail.com>
2021-12-05 14:31:35 +0200

    docs: add Rocket search preset, watch CSS files

--- rocket.config.js
+++ rocket.config.js
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
 import { rocketLaunch } from '@rocket/launch';
+import { rocketSearch } from '@rocket/search';
 
 export default {
   absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
-  presets: [rocketLaunch()]
+  presets: [rocketLaunch(), rocketSearch()],
+  eleventy(eleventyConfig) {
+    eleventyConfig.addWatchTarget('docs/_assets/**/*.css');
+  }
 };
