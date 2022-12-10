f67919c833d4fdf9d493d771491e3caa8a268ed9
Serhii Kulykov <iamkulykov@gmail.com>
2022-12-10 20:57:51 +0200

    chore: update postcss-lit and Stylelint versions (#166)

--- packages/api-viewer/src/styles.ts
+++ packages/api-viewer/src/styles.ts
@@ -3,14 +3,15 @@ import sharedStyles from '@api-viewer/common/lib/shared-styles.js';
 import demoStyles from '@api-viewer/demo/lib/styles.js';
 import docsStyles from '@api-viewer/docs/lib/styles.js';
 
-export default css`
-  ${sharedStyles}
-  ${docsStyles}
-  ${demoStyles}
-
-  [part='radio-label'] {
-    margin: 0 0.75rem 0 0.25rem;
-    color: var(--ave-header-color);
-    font-size: 0.875rem;
-  }
-`;
+export default [
+  sharedStyles,
+  docsStyles,
+  demoStyles,
+  css`
+    [part='radio-label'] {
+      margin: 0 0.75rem 0 0.25rem;
+      color: var(--ave-header-color);
+      font-size: 0.875rem;
+    }
+  `
+];
